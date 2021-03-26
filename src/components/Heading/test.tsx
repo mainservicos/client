542c2ea876054e0cf8468a3513c3620dda93a1ad
Venicius Alves <venicius.alves@gmail.com>
2021-03-26 00:00:02 -0300

    correção de teste

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -22,7 +22,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #2A9D8E'
     })
   })
 
@@ -30,7 +30,7 @@ describe('<Heading />', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
       'border-bottom',
-      '0.5rem solid #F231A5',
+      '0.5rem solid #FA9442',
       {
         modifier: '::after'
       }
