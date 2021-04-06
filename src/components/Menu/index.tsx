cb328e0c15c04e26be72d5af28beffa02d1de6d0
Venicius <venicius.alves@gmail.com>
2021-04-06 15:23:40 -0300

    Ajustes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -29,7 +29,7 @@ const Menu = ({ username }: MenuProps) => {
       </S.LogoWrapper>
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="/">Home</S.MenuLink>
           <S.MenuLink href="#">Explorar</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
---
+++ src/pages/service/[slug].tsx
@@ -0,0 +1,5 @@
+import Service from 'templates/Service'
+
+export default function Index() {
+  return <Service />
+}
